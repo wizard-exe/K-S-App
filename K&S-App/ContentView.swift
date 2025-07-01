746ef14426397293e3ca2eacb84042e8f0cca8be
Maximilian Krug <58868250+wizard-exe@users.noreply.github.com>
2025-07-01 16:13:12 +0200

    Update ContentView.swift

--- K&S-App/ContentView.swift
+++ K&S-App/ContentView.swift
@@ -1582,7 +1582,7 @@ struct SettingsView: View {
                 HStack {
                     Text("Version")
                     Spacer()
-                    Text("1.0.0")
+                    Text("1.0.2")
                         .foregroundColor(.secondary)
                 }
             }
